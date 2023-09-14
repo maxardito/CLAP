1a9fac160b6976acf99af1283fcf84ce70454946
maxardito <maxwellardito@gmail.com>
2023-09-14 19:18:48 -0400

    Changed paths to pth and wav test file

--- inference.py
+++ inference.py
@@ -1,13 +1,13 @@
 from src.models.clap import CLAP
 from src.CLAPWrapper import CLAPWrapper
 
-clap_model = CLAPWrapper(model_fp="CLAP.pth", use_cuda=True)
+clap_model = CLAPWrapper(model_fp="../CLAP.pth", use_cuda=True)
 
 text_prompts = [
     "drums", "hip hop", "uk garage", "bass", "orchestra", "acoustic",
     "electro", "70s", "the", "help", "republican"
 ]
-audio_file = "UK2S_130_Drum_Goin_Full.wav"
+audio_file = "../UK2S_130_Drum_Goin_Full.wav"
 
 text_embeddings = clap_model.get_text_embeddings(text_prompts)
 audio_embeddings = clap_model.get_audio_embeddings([audio_file], 1)
