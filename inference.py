137631179481c8105ba218a66aa5be4344676985
Julian Armand Vanasse <julian.vanasse@gmail.com>
2023-09-19 17:41:26 +0000

    finished CLAP inference pipeline

--- inference.py
+++ inference.py
@@ -1,21 +1,61 @@
+import os
+import json
+import tqdm
+import soundfile as sf
+
+AUDIO_FILETYPES = tuple(["." + filetype.lower() for filetype in list(sf._formats.keys())] + [".mp3"])
+
 from src.models.clap import CLAP
 from src.CLAPWrapper import CLAPWrapper
 
-clap_model = CLAPWrapper(model_fp="../CLAP.pth", use_cuda=True)
+CLAP_MODEL = "../CLAP.pth"
+KEYWORDS_FILE = "keywords.txt"
+AUDIO_DIR = "../audio/points005"
 
-text_prompts = [
-    "drums", "hip hop", "uk garage", "bass", "orchestra", "acoustic",
-    "electro", "70s", "the", "help", "republican"
-]
-audio_file = "../UK2S_130_Drum_Goin_Full.wav"
+clap_model = CLAPWrapper(model_fp=CLAP_MODEL, use_cuda=True)
 
-text_embeddings = clap_model.get_text_embeddings(text_prompts)
-audio_embeddings = clap_model.get_audio_embeddings([audio_file], 1)
+prompts = open(KEYWORDS_FILE).read().splitlines()
+prompt_batch_size = 1000
+num_prompts = len(prompts)
+# num_prompts = prompt_batch_size
 
-similarity = clap_model.compute_similarity(text_embeddings=text_embeddings,
-                                           audio_embeddings=audio_embeddings)
 
-similarity = similarity.cpu().detach().numpy()[0]
+## FOR each file:
+# audio_file = os.path.join(AUDIO_DIR, "06. LTJ Bukem - Music.mp3")
 
-for prompt, sim in list(zip(text_prompts, similarity)):
-    print(f"{prompt}: {sim}")
+for root, subdirs, files in os.walk(AUDIO_DIR):
+    for file in files:
+        if os.path.splitext(file)[-1].lower() in AUDIO_FILETYPES:
+            file = os.path.join(root, file)
+            basename = os.path.basename(file)
+            basename = os.path.splitext(basename)[0]
+            audio_embeddings = clap_model.get_audio_embeddings([file], 1)
+
+            results = []
+
+            for prompt_batch_start in tqdm.tqdm(range(0, num_prompts, prompt_batch_size)):
+                # get a batch of prompts
+                if prompt_batch_start + prompt_batch_size < num_prompts:
+                    prompt_batch = prompts[prompt_batch_start:prompt_batch_start+prompt_batch_size]
+                else:
+                    prompt_batch = prompts[prompt_batch_start:num_prompts]
+
+                text_embeddings = clap_model.get_text_embeddings(prompt_batch)
+
+                similarity = clap_model.compute_similarity(text_embeddings=text_embeddings,
+                                                        audio_embeddings=audio_embeddings)
+
+                similarity = similarity.cpu().detach().numpy()[0]
+
+                batch_results = [
+                    {
+                        "prompt": prompt, 
+                        "value": str(sim)
+                    } 
+                    for prompt, sim in list(zip(prompts, similarity))
+                    ]
+
+                results = results + batch_results
+
+            with open(f'{basename}.json', 'w') as f:
+                json.dump(results, f, indent="\t")
